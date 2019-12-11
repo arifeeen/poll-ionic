57d0e4db635b35ee1b1c3ac706d97fd46b47f5ac
unknown <sam.arif1111@gmail.com>
2019-12-11 21:46:09 +0530

    added show results functionality

--- src/app/dashboard/show-poll/show-results/show-results.page.ts
+++ src/app/dashboard/show-poll/show-results/show-results.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+import { PollService } from 'src/app/services/poll.service';
 
 @Component({
   selector: 'app-show-results',
@@ -6,10 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show-results.page.scss'],
 })
 export class ShowResultsPage implements OnInit {
+  pollId;
+  pollObject;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private loadingController: LoadingController, private pollService: PollService) { }
 
   ngOnInit() {
+    this.activatedRoute.paramMap.subscribe(params => {
+      if (params.has('pollId')) {
+        this.pollId = params.get('pollId');
+        this.loadingController.create({
+          keyboardClose: true,
+          message: 'loading data'
+        }).then(load => {
+          load.present();
+          this.pollService.getPoll(this.pollId).subscribe(res => {
+            load.dismiss();
+            this.pollObject = res.body;
+            console.log('res body', this.pollObject);
+          });
+
+        });
+      }
+
+    })
   }
 
 }
